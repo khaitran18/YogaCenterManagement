6e71b627fba48b228176f618402e4c46da2ac9ae
khaitran18 <khaitranquang987@gmail.com>
2023-07-02 22:20:57 +0700

    init User Detail

---
+++ View/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace View.Controllers
+{
+    [Route("User")]
+    public class UserController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private string apiUrl = "";
+        public UserController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            apiUrl = "https://localhost:7241/api";
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
