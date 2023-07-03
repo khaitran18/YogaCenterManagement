0430dbeb79e906286f57a70849a33ed09d892e55
nguyenntse <nguyenntse161785@fpt.edu.vn>
2023-07-03 16:51:21 +0700

    update

---
+++ View/Controllers/ClassController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+using View.Models;
+using View.Models.Response;
+
+namespace View.Controllers
+{
+    public class ClassController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private string apiUrl = "";
+        public ClassController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            apiUrl = "https://localhost:7241/api/class";
+        }
+
+        private string? GetAuthTokenFromCookie()
+        {
+            var authToken = Request.Cookies["AuthToken"];
+            return authToken;
+        }
+
+        private void AddAuthTokenToRequestHeaders()
+        {
+            var authToken = GetAuthTokenFromCookie();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+        }
+
+        public async Task<IActionResult> Index(string? searchKeyword, string? sortBy, DateTime? startingFromDate, int? durationMonths, string? classCapacity, int? page, int? pageSize)
+        {
+            var queryString = BuildQueryStringClasses(searchKeyword, sortBy, startingFromDate, durationMonths, classCapacity, page, pageSize);
+            var queryStringWithoutPage = RemoveQueryStringParameter(queryString, "page");
+            var url = apiUrl + queryString;
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                var baseResponse = JsonSerializer.Deserialize<BaseResponse<PaginatedResult<ClassDto>>>(responseBody, options);
+
+                if (!baseResponse!.Error)
+                {
+                    ViewBag.QueryString = queryStringWithoutPage;
+                    return View(baseResponse.Result);
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = baseResponse.Message;
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        private string BuildQueryStringClasses(string? searchKeyword, string? sortBy, DateTime? startingFromDate, int? durationMonths, string? classCapacity, int? page, int? pageSize)
+        {
+            var queryString = "";
+
+            if (!string.IsNullOrEmpty(searchKeyword))
+            {
+                queryString += $"searchKeyword={searchKeyword}&";
+            }
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                queryString += $"sortBy={sortBy}&";
+            }
+
+            if (startingFromDate != null)
+            {
+                queryString += $"startingFromDate={startingFromDate}&";
+            }
+
+            if (durationMonths != null)
+            {
+                queryString += $"durationMonths={durationMonths}&";
+            }
+
+            if (!string.IsNullOrEmpty(classCapacity))
+            {
+                queryString += $"classCapacity={classCapacity}&";
+            }
+
+            if (page != null)
+            {
+                queryString += $"page={page}&";
+            }
+
+            if (pageSize != null)
+            {
+                queryString += $"pageSize={pageSize}&";
+            }
+
+            if (queryString.EndsWith("&"))
+            {
+                queryString = queryString.TrimEnd('&');
+            }
+
+            if (queryString != "")
+            {
+                queryString = "?" + queryString;
+            }
+
+            return queryString;
+        }
+
+        private string RemoveQueryStringParameter(string queryString, string key)
+        {
+            var collection = HttpUtility.ParseQueryString(queryString);
+            collection.Remove(key);
+            return "?" + collection.ToString();
+        }
+    }
+}
